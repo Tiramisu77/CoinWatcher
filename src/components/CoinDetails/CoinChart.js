8572b699ba8131fdb1d04989a50f58ba6f9ca8e9
Tiramisu77 <misamitsu@protonmail.com>
2019-11-22 16:45:04 +0200

    improve chart

--- src/components/CoinDetails/CoinChart.js
+++ src/components/CoinDetails/CoinChart.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import { loadHistoricalPrices } from "@app/network"
 
 const Chart = require("chart.js")
@@ -9,110 +9,94 @@ function beforePrintHandler() {
     }
 }
 
-class ChartView extends React.Component {
-    constructor(props) {
-        super(props)
-
-        window.addEventListener("beforeprint", beforePrintHandler)
-    }
-
-    renderChart() {
-        console.log(this.props)
-        const node = this.node
-
-        new Chart(node, {
-            type: "line",
-            data: {
-                datasets: [
-                    {
-                        label: `price ${this.props.label}`,
-                        data: this.props.data.prices,
-                        backgroundColor: [
-                            "rgba(255, 99, 132, 0.2)",
-                            "rgba(54, 162, 235, 0.2)",
-                            "rgba(255, 206, 86, 0.2)",
-                            "rgba(75, 192, 192, 0.2)",
-                            "rgba(153, 102, 255, 0.2)",
-                            "rgba(255, 159, 64, 0.2)",
-                        ],
-                        borderColor: [
-                            "rgba(255,99,132,1)",
-                            "rgba(54, 162, 235, 1)",
-                            "rgba(255, 206, 86, 1)",
-                            "rgba(75, 192, 192, 1)",
-                            "rgba(153, 102, 255, 1)",
-                            "rgba(255, 159, 64, 1)",
-                        ],
-                        borderWidth: 1,
-                    },
-                    {
-                        label: `value ${this.props.label}`,
-                        data: this.props.data.amounts,
-                        backgroundColor: [
-                            "rgba(72, 47, 156, 0.2)",
-                            "rgba(54, 162, 235, 0.2)",
-                            "rgba(255, 206, 86, 0.2)",
-                            "rgba(75, 192, 192, 0.2)",
-                            "rgba(153, 102, 255, 0.2)",
-                            "rgba(255, 159, 64, 0.2)",
-                        ],
-                        borderColor: [
-                            "rgba(72, 47, 156, 1)",
-                            "rgba(54, 162, 235, 1)",
-                            "rgba(255, 206, 86, 1)",
-                            "rgba(75, 192, 192, 1)",
-                            "rgba(153, 102, 255, 1)",
-                            "rgba(255, 159, 64, 1)",
-                        ],
-                        borderWidth: 1,
-                        hidden: true,
-                    },
-                ],
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                scales: {
-                    xAxes: [
+function ChartView({ data, unit, label }) {
+    const canvas = useRef(null)
+    useEffect(
+        () => {
+            window.addEventListener("beforeprint", beforePrintHandler)
+            new Chart(canvas.current, {
+                type: "line",
+                data: {
+                    datasets: [
                         {
-                            type: "time",
-                            time: {
-                                unit: this.props.unit,
-                            },
-                            ticks: {
-                                suggestedMin: 0,
-                                maxTicksLimit: 8,
-                            },
+                            label: `price ${label}`,
+                            data: data.prices,
+                            backgroundColor: [
+                                "rgba(255, 99, 132, 0.2)",
+                                "rgba(54, 162, 235, 0.2)",
+                                "rgba(255, 206, 86, 0.2)",
+                                "rgba(75, 192, 192, 0.2)",
+                                "rgba(153, 102, 255, 0.2)",
+                                "rgba(255, 159, 64, 0.2)",
+                            ],
+                            borderColor: [
+                                "rgba(255,99,132,1)",
+                                "rgba(54, 162, 235, 1)",
+                                "rgba(255, 206, 86, 1)",
+                                "rgba(75, 192, 192, 1)",
+                                "rgba(153, 102, 255, 1)",
+                                "rgba(255, 159, 64, 1)",
+                            ],
+                            borderWidth: 1,
+                        },
+                        {
+                            label: `value ${label}`,
+                            data: data.amounts,
+                            backgroundColor: [
+                                "rgba(72, 47, 156, 0.2)",
+                                "rgba(54, 162, 235, 0.2)",
+                                "rgba(255, 206, 86, 0.2)",
+                                "rgba(75, 192, 192, 0.2)",
+                                "rgba(153, 102, 255, 0.2)",
+                                "rgba(255, 159, 64, 0.2)",
+                            ],
+                            borderColor: [
+                                "rgba(72, 47, 156, 1)",
+                                "rgba(54, 162, 235, 1)",
+                                "rgba(255, 206, 86, 1)",
+                                "rgba(75, 192, 192, 1)",
+                                "rgba(153, 102, 255, 1)",
+                                "rgba(255, 159, 64, 1)",
+                            ],
+                            borderWidth: 1,
+                            hidden: true,
                         },
                     ],
                 },
-                elements: {
-                    point: {
-                        radius: 0,
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    scales: {
+                        xAxes: [
+                            {
+                                type: "time",
+                                time: {
+                                    unit: unit,
+                                },
+                                ticks: {
+                                    suggestedMin: 0,
+                                    maxTicksLimit: 8,
+                                },
+                            },
+                        ],
+                    },
+                    elements: {
+                        point: {
+                            radius: 0,
+                        },
                     },
                 },
-            },
-        })
-    }
+            })
 
-    componentDidMount() {
-        this.renderChart()
-    }
-
-    componentDidUpdate() {
-        this.renderChart()
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("beforeprint", beforePrintHandler)
-    }
-    render() {
-        return (
-            <div className="chart-container">
-                <canvas style={{ width: 800, height: 300 }} ref={node => (this.node = node)} />
-            </div>
-        )
-    }
+            return () => window.removeEventListener("beforeprint", beforePrintHandler)
+        },
+        [data]
+    )
+    return (
+        <div className="chart-container">
+            <canvas style={{ width: 800, height: 300 }} ref={canvas} />
+        </div>
+    )
 }
 
 function getChartData({ id, mainCurrency, period, signal }) {
@@ -129,33 +113,36 @@ function getUnit(periodStr) {
 export function CoinChart({ id, mainCurrency, period, amount }) {
     const [data, setData] = useState(null)
 
-    useEffect(() => {
-        let controller = new AbortController()
-        let signal = controller.signal
+    useEffect(
+        () => {
+            let controller = new AbortController()
+            let signal = controller.signal
 
-        getChartData({ id, mainCurrency, period, signal })
-            .then(({ prices }) => {
-                prices = prices.map(arr => {
-                    let [x, y] = arr
-                    return { x: new Date(x), y }
+            getChartData({ id, mainCurrency, period, signal })
+                .then(({ prices }) => {
+                    prices = prices.map(arr => {
+                        let [x, y] = arr
+                        return { x: new Date(x), y }
+                    })
+                    let amounts = prices.map(({ x, y }) => {
+                        return { x, y: y * amount }
+                    })
+                    setData({ prices, amounts })
                 })
-                let amounts = prices.map(({ x, y }) => {
-                    return { x, y: y * amount }
+                .catch(e => {
+                    if (e.message === "The user aborted a request.") {
+                        return
+                    } else {
+                        setData("error")
+                    }
                 })
-                setData({ prices, amounts })
-            })
-            .catch(e => {
-                if (e.message === "The user aborted a request.") {
-                    return
-                } else {
-                    setData("error")
-                }
-            })
 
-        return function cleanup() {
-            controller.abort()
-        }
-    }, [])
+            return function cleanup() {
+                controller.abort()
+            }
+        },
+        [period]
+    )
     return (
         <>
             {data !== null && data !== "error" && <ChartView data={data} unit={getUnit(period)} label={mainCurrency} />}
